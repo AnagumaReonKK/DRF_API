db26a6c0823e2192ad75c6cd4c9b45a84fd9554f
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2021-11-27 18:25:53 +0900

    api/urls.py is error

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from rest_framework import routers
 from api.views import TaskViewSet, UserViewSet, ManageUserView
 
 router = routers.DefaultRouter()
-routers.register('tasks', TaskViewSet)
-routers.register('users', UserViewSet)
+router.register('tasks', TaskViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('myself/', ManageUserView.as_view(), name='myself'),
